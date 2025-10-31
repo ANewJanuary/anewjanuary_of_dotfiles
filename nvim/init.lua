a6c2834f468040fc27a712aa8987fc68c6e3bbb1
ANewJanuary <artin2007majidi@gmail.com>
2025-10-31 23:14:03 +0100

    migrated to vscode and used taskwarrior

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,20 @@
+-- ordinary Neovim
 require("config")
 require("config.lazy")
 -- treesitter stuff
 require("lazy").setup({
-  {"nvim-treesitter/nvim-treesitter", branch = 'main', lazy = false, build = ":TSUpdate"}
+    {"nvim-treesitter/nvim-treesitter", branch = 'main', lazy = false, build = ":TSUpdate"}
 })
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme solarized]])
+local group = vim.api.nvim_create_augroup("VCenterCursor", { clear = true })
+
+vim.api.nvim_create_autocmd(
+    {"BufEnter", "WinEnter", "WinNew", "VimResized"},
+    {
+        group = group,
+        pattern = {"*", "*.*"},
+        command = "let &scrolloff=(winheight(win_getid())/2) - (winheight(win_getid())/4)",
+    }
+)
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,7 +7,7 @@ vim.o.cursorline = true
 
 vim.opt.smartindent = true
 vim.opt.autoindent = true
-vim.opt.textwidth = 70
+vim.opt.textwidth = 65
 vim.opt.smarttab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -15,15 +15,13 @@ vim.opt.expandtab = true
 
 vim.opt.mouse = "a"
 vim.opt.showmode = false
-vim.opt.ruler = false
+vim.opt.ruler = true
 vim.opt.breakindent = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.signcolumn = "yes"
-
 vim.opt.scrolloff = 10
-
 vim.opt.laststatus=0
 
